e9a2bfaab64794ed1775dbb15fdcd270ebc4ade3
zelalemng <zedneg@gmail.com>
2023-02-16 13:37:54 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
-/* main - A C program that prints a line to standard error
+/**
+ * main - A c program that prints a line to standard error
  * Return 1 (Success)
  */
 int main(void)
