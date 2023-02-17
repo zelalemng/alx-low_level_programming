7270a1bfc58e74d123fb28ec4e9404bd78faa496
zelalemng <zedneg@gmail.com>
2023-02-17 14:45:42 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry points
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+	i = 0;
+	c = 'a';
+	while
+		(i < 10) {
+			putchar(i + '0');
+			i++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+
+}
