4ceeb0629af2cbd7e2702ecebd9b8c07c7b9bc85
zelalemng <zedneg@gmail.com>
2023-02-17 15:14:00 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,13 +4,16 @@
  *
  * return: Always 0 (success)
  */
+
 int main(void)
 {
 	int d;
 
-	for (d = 0; d < 10; d++) {
+	for (d = 0; d < 10; d++) 
+	{
 		putchar(d + '0');
-		if (d < 9) {
+		if (d < 9) 
+		{
 			putchar(',');
 			putchar(' ');
 		}
