60efeb411dab444c750fbfdbda56c57d3c4e960b
zelalemng <zedneg@gmail.com>
2023-02-17 14:10:57 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry points
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int z;
+
+	z = 0;
+	while
+		(z < 10) {
+			putchar(z + '0');
+			z++;
+
+		}
+	putchar('\n');
+	return (0);
+}
