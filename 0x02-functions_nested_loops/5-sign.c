828be3f81b8f64759e8f56e02f0472324c0634c1
zelalemng <zedneg@gmail.com>
2023-02-21 09:10:03 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - Prints the sign of a number
- * @c: The number to be checked
+ * @n: The number to be checked
  * Return: 1 for positive num, for negative num or zero for anything else
  */
 
