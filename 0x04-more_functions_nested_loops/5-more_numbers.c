07b95916b269c7d64c1e1f3ec737dcb49f80c896
zelalemng <zedneg@gmail.com>
2023-02-24 06:00:35 -0800

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,7 @@ void more_numbers(void)
 	{
 		while (start <= end)
 		{
-			_purchar(start > 9 ? (start / 10) + '0' : start + '0');
+			_putchar(start > 9 ? (start / 10) + '0' : start + '0');
 
 			if (start > 9)
 				_putchar((start % 10) + '0');
