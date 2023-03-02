eaee649b53d4bfb5fdceec8222b2ba54476c32d7
zelalemng <zedneg@gmail.com>
2023-03-02 13:11:32 -0800

    4-rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reverse_array - reverse the content of an array
+ * @a: The array of integers
+ * @n: The number of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+}
