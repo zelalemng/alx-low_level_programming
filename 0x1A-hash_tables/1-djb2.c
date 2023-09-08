9fc9d0fc279f01070749ca510d4e754b94709d1e
zelalemng <zedneg@gmail.com>
2023-09-08 15:09:29 -0700

    task_1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,17 +2,18 @@
 /**
  * hash_djb2 - hash function implementing the djb2 algorithm
  * @str: string to hash
+ *
  * Return: hashed value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-	    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
